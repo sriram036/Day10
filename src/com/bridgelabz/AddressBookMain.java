03e163137b66c555a06e641d33ea6786401c85cc
Ravi@036# <admin@admins-MacBook-Pro.local>
2023-04-17 00:27:13 +0530

    added: person details

--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,6 +2,18 @@ package com.bridgelabz;
 
 public class AddressBookMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to Day10 Address Book Program.");
+        Contact contact = new Contact();
+        AddressBook addressBook = new AddressBook();
+        addressBook.contactPerson1 = contact;
+
+        contact.setFirstName("Sriram");
+        contact.setLastName("Ravichandran");
+        contact.setCity("Bangalore");
+        contact.setState("Karnataka");
+        contact.setZip(560032);
+        contact.setPhoneNumber(1234567890);
+        contact.setEmail("sri123@gmail.com");
+
+        System.out.println(addressBook);
     }
 }
